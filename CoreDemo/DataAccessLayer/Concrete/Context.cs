f63752897bc1e0dd784d04a456b27db417286895
Okan3465 <okan34.karakoc@gmail.com>
2022-07-06 20:12:25 +0300

    06.07.2022
    Yazar Mesaj ve Bildirimleri

--- CoreDemo/DataAccessLayer/Concrete/Context.cs
+++ CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -25,6 +25,8 @@ namespace DataAccessLayer.Concrete
         public DbSet<Writer> Writers { get; set; }
         public DbSet<NewsLetter> NewsLetters { get; set; }
         public DbSet<BlogRaiting> BlogRaitings { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Message> Messages { get; set; }
 
 
     }
